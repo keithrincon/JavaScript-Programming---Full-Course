76b6d505c0a8b546b5f51834bed82d758743b321
Keith Rincon <keithrt3008@gmail.com>
2024-02-15 16:32:26 -0800

    Practice time completed

--- 4. Practice time - part 2/06. Rock papers scissors/index.js
+++ 4. Practice time - part 2/06. Rock papers scissors/index.js
@@ -1,5 +1,11 @@
-let hands = ["rock", "paper", "scissor"]
+let hands = ['rock', 'paper', 'scissor'];
 
 // Create a function that returns a random item from the array
 
+function getHand() {
+  let i = Math.floor(Math.random() * 3);
+  console.log(hands[i]);
+  console.log(i);
+}
 
+getHand();
