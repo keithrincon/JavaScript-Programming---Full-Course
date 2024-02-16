b0d3384d5a52eaa90bf37bad9a48eaf499306e5d
Keith Rincon <keithrt3008@gmail.com>
2024-02-15 21:20:11 -0800

    Building an extension

--- 5. Build a Chrome Extension/07. Your turn to refactor/index.js
+++ 5. Build a Chrome Extension/07. Your turn to refactor/index.js
@@ -1,9 +1,12 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
-function saveLead() {
-    console.log("Button clicked!")
-}
+// function saveLead() {
+//   console.log('Button clicked!');
+// }
 
+let saveInput = document.getElementById('input-btn');
 
-
+saveInput.addEventListener('click', function () {
+  console.log('Button clicked');
+});
