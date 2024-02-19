877fa2e38e3e33b1552e835f00cea3a60b1fd84c
Keith Rincon <keithrt3008@gmail.com>
2024-02-19 12:39:08 -0800

    Completed Course

--- 6. Practice time - part 3/03. save to localStorage/index.js
+++ 6. Practice time - part 3/03. save to localStorage/index.js
@@ -1,3 +1,9 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+
+// localStorage.setItem('myCredits', 'Kobes');
+
+let myCredits = localStorage.getItem('myCredits');
+
+console.log(myCredits);
--- 6. Practice time - part 3/04. addEventListener and object in array/index.js
+++ 6. Practice time - part 3/04. addEventListener and object in array/index.js
@@ -1,14 +1,20 @@
 let data = [
-    {
-        player: "Jane",
-        score: 52
-    }, 
-    {
-        player: "Mark",
-        score: 41
-    }
-]
+  {
+    player: 'Jane',
+    score: 52,
+  },
+  {
+    player: 'Mark',
+    score: 41,
+  },
+];
 
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
+scoreEl = document.getElementById('score-el');
+
+addEventListener('click', function () {
+  console.log('clicked');
+  console.log(`Jane scored a total of ${data[0].score} points`);
+});
